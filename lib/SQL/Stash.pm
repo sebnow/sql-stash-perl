ece4c8846bb065e76d96cba8d65b03b567c3fc68
Sebastian Nowicki <sebnow@gmail.com>
2012-01-31 19:57:08 +0800

    Implement SQL::Stash->new()

--- lib/SQL/Stash.pm
+++ lib/SQL/Stash.pm
@@ -3,9 +3,17 @@ use strict;
 use warnings;
 
 use v5.6;
+use Carp qw(croak);
 use version v0.77;
 
 our $VERSION = version->declare("v0.0.0");
+
+sub new {
+	my ($class, %args) = @_;
+	my $self = bless({}, $class);
+	$self->{'dbh'} = $args{'dbh'} or croak("DBI handle missing");
+	return $self;
+}
 
 1;
 
@@ -25,8 +33,21 @@ prepare these statements as late as possible (i.e. before they are
 executed).
 
 SQL::Stash is in concept very similar to L<Ima::DBI|Ima::DBI>, but
-differs by having instance-specific database handle, and by supporting
-l<ResourcePool|ResourcePool>.
+differs by having instance-specific database handles and statements, and
+by supporting externally defined database handles.
+
+=head1 METHODS
+
+=head2 new
+
+	SQL::Stash->new(%args);
+
+Designated constructor. Instantiates a new L<SQL::Stash|SQL::Stash>
+object. The C<dbh> argument, a L<DBI|DBI>-like object,  must be
+provided.
+
+	my $dbh = DBI->connect('dbi:SQLite:dbname=:memory:', '', '');
+	my $stash = SQL::Stash->new('dbh' => $dbh);
 
 =head1 SEE ALSO
 
--- t/SQL/Stash.t
+++ t/SQL/Stash.t
@@ -2,9 +2,21 @@
 use strict;
 use warnings;
 
-use Test::More 'tests' => 1;
+use Test::Exception;
+use Test::MockModule;
+use Test::More 'tests' => 2;
 
 BEGIN {
-	use_ok('SQL::Stash');
+	use_ok('SQL::Stash') or BAIL_OUT('Unable to use SQL::Stash');
 };
 
+subtest "Given the SQL::Stash class" => sub {
+	my $mock_dbi = Test::MockModule->new('DBI');
+	$mock_dbi->mock('connect', sub {return bless({}, shift)});
+	throws_ok(sub {SQL::Stash->new()}, qr/DBI handle missing/,
+		"when the class is instantiated and a DBI handle is not provided ".
+		"then an exception should be thrown");
+	lives_ok(sub {SQL::Stash->new('dbh' => DBI->connect())},
+		"when the class is instantiated and a DBI handle is provided ".
+		"then an an instance should be created");
+};
